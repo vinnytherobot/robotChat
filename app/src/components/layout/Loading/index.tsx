68eee4e4fc5d9e02b28f5d5193b2a2e38ca4aa10
João Vinny <119421047+vinnyrobot@users.noreply.github.com>
2023-01-14 00:20:58 +0000

    Update

--- app/src/components/layout/Loading/index.tsx
+++ app/src/components/layout/Loading/index.tsx
@@ -1,11 +1,26 @@
-import loading from "../../../assets/loading.svg";
+
 import styles from "./index.module.css";
 
 function Loading(){
     return(
-        <>
-            <img className={styles.loading} src={loading}/>
-        </>
+        <div className={styles.loading}>
+            <div className={styles.loadingioSpinnerSpinnerHag5ogxmu8l}>
+                <div className={styles.ldio05qbti1k1yv6}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
